b79ef8db4a41342bd3f4582a461431ef556f8828
Katie Taruna <katie.taruna@rubicotech.in>
2022-05-19 23:39:57 +0530

    fetch and display tasks on home

---
+++ src/components/SingleTask.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="task">
+      <div class="actions">
+          <h3>{{task.title}}</h3>
+      </div>
+      <div class="details">
+          <p>{{task.details}}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'task'
+    ]
+}
+</script>
+
+<style>
+
+  .task {
+      margin: 20px auto;
+      background: white;
+      padding: 10px 20px;
+      border-radius: 4px;
+      box-shadow: 1px 2px 3px rgb(0,0,0,0.5);
+      border-left: 4px solid red;
+  }
+  
+  h3 {
+      cursor: pointer;
+  }
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="home">
-    Home
+    <div v-if="tasks.length">
+      <div v-for="task in tasks" :key="task.id">
+        <single-task :task="task" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import SingleTask from '../components/SingleTask.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    
-  }
+    SingleTask
+  },
+
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  mounted() {
+    //local json server route
+    fetch('http://localhost:3000/tasks')
+      .then(res => res.json())
+      .then(data => this.tasks = data)
+      .catch(error => console.log(error.message))
+  },
 }
 </script>
